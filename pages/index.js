6d148801614dc21092fc2860c6259692b26591a8
jyotirsai <jyotirsai28@gmail.com>
2020-09-22 19:22:28 -0600

    added fetch

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
+import { useState } from "react";
 import Head from "next/head";
 
-export default function Home({ data }) {
+export default function Home() {
+  const [location, setLocation] = useState("");
+  const [saveLocation, setSaveLocation] = useState("");
+
+  function handleChange(event) {
+    setLocation(event.target.value);
+  }
+
+  async function handleClick() {
+    const res = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Edmonton&units=metric&appid=bac3f7168a13a53749b5aaf75fed3634"
+    );
+    const data = await res.json();
+    setSaveLocation(data.main.temp);
+  }
+
   return (
     <div>
       <Head>
         <title>Weather App</title>
       </Head>
-      <h1>hello world</h1>
-      <p>{data.main.temp}</p>
+      <h1>Weather App</h1>
+      <input value={location} onChange={handleChange} />
+      <button onClick={handleClick}>Find Weather</button>
+      <p>{saveLocation}</p>
     </div>
   );
-}
-
-// Call this function on every request
-export async function getServerSideProps() {
-  // fetch data from API
-  const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Edmonton&units=metric&appid=bac3f7168a13a53749b5aaf75fed3634"
-  );
-  const data = await res.json();
-
-  return { props: { data } };
 }
